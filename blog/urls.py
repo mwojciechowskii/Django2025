d8cf6655040df9f0080e9a67a6f7f746601d935f
mwojciechowskii <mwojciechowskii@proton.me>
2026-01-17 14:43:38 +0100

    blog post detail add

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.postListView, name="postListView"),
+    path('category/<categoryName>/', views.postListView, name="postListCategory"),
+    path('post/<int:postID>', views.postDetailView, name="postDetailView"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Post
+from .models import Category, Post
 
-def postListView(request):
+def postListView(request, categoryName=None):
 
-    queryset = Post.objects.filter(isPublic=True).order_by("-datePublish")
-    context = {
-        'objectList': queryset
-    }
+    if categoryName:
+        queryset = Post.objects.filter(isPublic=True, categoryName=categoryName)
+    else:
+        queryset = Post.objects.all()
+
+    queryset = queryset.filter(isPublic=True).order_by("-datePublish")
+    context = { 'categoryList': Category.objects.all(),
+        'objectList': queryset,
+        'categoryName': categoryName}
+
     return render(request, 'blog/postList.html', context)
+
+def postDetailView(request, postID):
+    
+    post = get_object_or_404(Post, id=postID)
+    context = { 'object': post,
+        'categoryList': Category.objects.all(),
+        'categoryName': post.category.name if post.category else 'Uncategorized' }
+
+    return render(request, 'blog/postDetail.html', context)
+
